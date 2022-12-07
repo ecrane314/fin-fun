bdfd702bb26369740441acd6de23056a50a740f5
Evan Crane <ecrane314@gmail.com>
2022-12-07 17:01:24 -0600

    adding notes

--- get-FRED.py
+++ get-FRED.py
@@ -1,4 +1,12 @@
 import requests
 
+# https://fred.stlouisfed.org/docs/api/fred/series.html is API Doc for Series
+# Go to page of your series and snag the ID from the URL, DGS2# is 2 Year Treasuries, Daily
+# Key is .gitignored in local file api.key, grab from your Fred account
+# https://www.w3schools.com/python/ref_requests_get.asp 
+
 https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=<ADD API KEY FROM PASSWORDS>&file_type=json
 
+two_year = requests.Request()
+
+= GEt
