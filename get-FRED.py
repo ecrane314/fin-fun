4336fa5032c22db0abde0b96b0b02d8dd7d1ad24
Evan Crane <ecrane314@gmail.com>
2022-12-09 16:17:34 -0600

    working pull of interest rates

--- get-FRED.py
+++ get-FRED.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python
+
+import os
 import requests
 
-# https://fred.stlouisfed.org/docs/api/fred/series.html is API Doc for Series
-# Go to page of your series and snag the ID from the URL, DGS2# is 2 Year Treasuries, Daily
+# https://fred.stlouisfed.org/docs/api/fred/#API see fred/series and fred/series/observations
+# Go to page of your series and snag the ID from the URL, DGS2 is 2 Year Treasuries, Daily
 # Key is .gitignored in local file api.key, grab from your Fred account
-# https://www.w3schools.com/python/ref_requests_get.asp 
 
-https://api.stlouisfed.org/fred/series?series_id=GNPCA&api_key=<ADD API KEY FROM PASSWORDS>&file_type=json
 
-two_year = requests.Request()
+# Pull in local API key from FRED account, no whitespace
+f = open('FRED-api.key', 'r')
+key = f.read()
 
-= GEt
+out = open('requests.temp', 'a')
+
+r = requests.get(f'https://api.stlouisfed.org/fred/series?series_id=DGS2&api_key={key}&file_type=json')
+out.write(r.text)
+
+r2 = requests.get((f'https://api.stlouisfed.org/fred/series/observations?series_id=DGS2&api_key={key}&file_type=json'))
+out.write(r2.text)
+
+out.close()
