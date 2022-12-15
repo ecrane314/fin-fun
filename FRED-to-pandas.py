a792adfc3d0e24d8e5b80fde7ad5d16c58516065
Evan Crane <ecrane314@gmail.com>
2022-12-15 14:42:32 -0600

    shebang

--- FRED-to-pandas.py
+++ FRED-to-pandas.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import requests
 from pandas.io.json import json_normalize
 import pandas as pd
@@ -6,4 +8,5 @@ import pandas as pd
 
 url = "https://api.exchangerate-api.com/v4/latest/USD"
 df = pd.read_json(url)
+
 print(df)
