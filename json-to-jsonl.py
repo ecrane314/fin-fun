29c7bf33f1c5333bf4f74f1db4fb57bc8c17df2d
Evan Crane <ecrane314@gmail.com>
2023-01-15 04:38:27 -0600

    fixing dump logic

--- json-to-jsonl.py
+++ json-to-jsonl.py
@@ -6,15 +6,20 @@
 #import pandas as pd
 import json
 
-filename = 'requests.temp'
+in_file = 'requests.temp'
+out_file = 'requsts.jsonl.temp'
 
 # Open json file for read
-f = open(filename, 'r')
+f = open(in_file, 'r')
 
 # Load json for text or binary files, object will make dict
 # https://docs.python.org/3/library/json.html#json.load
 json_input = json.load(f)
 
-#TODO Encode dict as jsonl?? Or is jsonl how I would dump it?
-
-#TODO MAYBE needed json.dump() to file
+#TODO BROKEN
+# Write entries from dict per line to achieve jsonl
+out = open(out_file, 'w')
+for i in json_input:
+    json_out = json.dumps(i) + '\n'
+    out.write(json_out)
+out.close()
