a7e54eea91b37ad21be1dd3a356f1ea3cacef3bb
Evan Crane <ecrane314@mgmail.com>
2025-05-20 20:30:14 -0500

    need topic schema

---
+++ polygon_rest_option_chain.py
@@ -0,0 +1,52 @@
+import os
+api_key = os.environ.get('apiKey')
+
+from polygon import RESTClient
+
+client = RESTClient(api_key=api_key)
+
+ticker = "AAPL"
+
+
+
+contract = client.get_options_contract(
+	"O:SPY251219C00650000"
+	)
+print(contract)
+
+
+""" contracts = []
+for c in client.list_options_contracts(
+	order="asc",
+	limit=10,
+	sort="ticker",
+	):
+    contracts.append(ticker)
+
+print(contracts)
+ """
+
+""" 
+# List Aggregates (Bars)
+aggs = []
+for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
+    aggs.append(a)
+print(aggs)
+
+# Get Last Trade
+trade = client.get_last_trade(ticker=ticker)
+print(trade)
+
+# List Trades
+trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
+for trade in trades:
+    print(trade)
+
+# Get Last Quote
+quote = client.get_last_quote(ticker=ticker)
+print(quote)
+
+# List Quotes
+quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
+for quote in quotes:
+    print(quote) """
