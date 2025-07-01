de6da1486bf1f8fbe5a41f966e3983b6dd716b11
Evan Crane <ecrane314@gmail.com>
2025-07-01 15:41:01 -0500

    Delete config.sh

--- config.sh
+++
@@ -1,6 +0,0 @@
-source .venv/bin/activate
-export POLYGON_API_KEY="projects/990799180178/secrets/polygon/versions/1"
-export REQUESTS_SUBSCRIPTION="projects/990799180178/subscriptions/polygon_requested_ticker_chain"
-export definition_file="./option_contract_pubsub_schema.json"
-export REQUESTS_TOPIC="polygon_requested_ticker_chain"
-export BIGQUERY_DEST="ce-demo1.FRED_landing"
