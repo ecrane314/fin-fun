4f4d9758b93dd586ca123d09c7e4ccdcc6ffd529
Evan Crane <ecrane314@mgmail.com>
2025-06-30 21:33:52 -0500

    contract overview working

--- bootstrap_gcp_polygon.sh
+++ bootstrap_gcp_polygon.sh
@@ -1,12 +1,19 @@
 #bootstrap
 
+# Create this in a config.sh file and source it. 
+# export POLYGON_API_KEY="projects/<PROJ NUM>/secrets/polygon/versions/1"
+# export REQUESTS_SUBSCRIPTION="projects/<PROJ NUM>/subscriptions/polygon_requested_ticker_chain"
+# export definition_file="./option_contract_pubsub_schema.json"
+# export REQUESTS_TOPIC="polygon_requested_ticker_chain"
+# export BIGQUERY_DEST="bq table name URI here"
+
 source ./config.sh
 
 gcloud auth application-default login
 
 gcloud pubsub schemas create option_contract_schema \
     --type=JSON \
-    --definition-file= $definition-file
+    --definition-file= $definition_file
 
 gcloud pubsub topic create -- use schema $REQUESTS_TOPIC
 
--- polygon_list_option_contract_overview.py
+++ polygon_list_option_contract_overview.py
@@ -14,7 +14,8 @@ def key_from_secret_manager():
     '''retrieve key from GCP Secret Manager
     return type str'''
     with secretmanager.SecretManagerServiceClient() as client:
-        response_key = client.access_secret_version(request= "name": os.environ.get('POLYGON_API_KEY'))
+        response_key = client.access_secret_version(request={"name": \
+                os.environ.get('POLYGON_API_KEY')})
         key = response_key.payload.data.decode("UTF-8")
     return key
 
@@ -23,7 +24,7 @@ def key_from_secret_manager():
 #TODO create pubsub client
 pubsub_client = pubsub_v1.SubscriberClient()
 def pull_from_subscription():
-     #TODO get a ticker from pubsub
+    '''TODO get a ticker from pubsub'''
     result = pubsub_client.pull(REQUESTS_SUBSCRIPTION)
     return result
 
@@ -34,31 +35,13 @@ def acknowledge_message(subscription_id, message_id):
 
 
 polygon_client = RESTClient(api_key=key_from_secret_manager())
-def get_option_chain_from_polygon(ticker, results_limit=10):
-# https://polygon.io/docs/rest/options/snapshots/option-chain-snapshot
-    options_chain = []
-    for o in polygon_client.list_snapshot_options_chain(
-        ticker,
-        params={
-            "order": "asc",
-            "limit": results_limit,
-            "sort": "ticker"
-        }
-	):
-        options_chain.append(o)
-    return options_chain
+def get_option_contract_overview():
+    contract = polygon_client.get_options_contract(
+        "O:SPY251219C00650000"
+        )
+    print(contract)
 
 
 
 if __name__ == "__main__":
-    try:
-        target_ticker = pull_from_subscription()
-
-        response = get_option_chain_from_polygon(target_ticker)
-        if response.status == OK:
-            acknowledge_message(response.messageID)
-
-        print(response)
-
-    except:
-        print ("Could not pull from subscription")
+    get_option_contract_overview()
